735d35a01519a94d5f9f96580b9300b8ecc6d8ae
Rob <codinwithrobz@gmail.com>
2022-10-18 18:26:00 -0600

    all routes on app

---
+++ src/router/routes.js
@@ -0,0 +1,20 @@
+//store all of the potential routes
+//add or remove routes as necessary
+export const routes = [
+  {
+    id: "home",
+    name: "Home",
+  },
+  {
+    id: "food",
+    name: "Food Menu",
+  },
+  {
+    id: "drinks",
+    name: "Drink Menu",
+  },
+  {
+    id: "contact",
+    name: "Contact Us",
+  },
+];
